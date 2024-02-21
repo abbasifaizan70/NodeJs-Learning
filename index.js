d88404dbe03263cf89edc01c0e3a24dcd7ce5921
Faizan Abbasi <abbasi.faizan1997@gmail.com>
2024-02-21 15:45:14 +0500

    Added switch case to hanlde url

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const http = require('http');
-const fs = require('fs')
+const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
   const log = `${Date.now()} ${req.url} New request has been recieved\n`;
-  fs.appendFile('logs.txt', log, (err, data) => {
-    res.end("Ended");
-  })
+  fs.appendFile("logs.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Home Page");
+        break;
+      case "/about":
+        res.end("about Page");
+        break;
+      case "/contact":
+        res.end("contact Page");
+        break;
+    }
+  });
   // console.log('My requst');
-  
-})
+});
 
 myServer.listen(8000, () => {
-  console.log('My server running')
-})
+  console.log("My server running");
+});
