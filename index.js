79f94bde8a748347745176ce39f70b91a66aebb1
Faizan Abbasi <abbasi.faizan1997@gmail.com>
2024-02-21 17:30:05 +0500

    Install npm(url) and see different ways to play with url

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const http = require("http");
 const fs = require("fs");
+const url  = require('url')
 
 const myServer = http.createServer((req, res) => {
   const log = `${Date.now()} ${req.url} New request has been recieved\n`;
+  const myUrl = url.parse(req.url, true)
+  console.log(myUrl)
   fs.appendFile("logs.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("Home Page");
         break;
       case "/about":
-        res.end("about Page");
+        const myName = myUrl.query.name;
+        res.end(`Hi ${myName}`);
         break;
       case "/contact":
         res.end("contact Page");
@@ -18,6 +22,7 @@ const myServer = http.createServer((req, res) => {
   });
   // console.log('My requst');
 });
+
 
 myServer.listen(8000, () => {
   console.log("My server running");
