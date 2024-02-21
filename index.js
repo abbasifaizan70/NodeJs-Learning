56a261bcf51e9c7525530bcfb027e90901fd9b09
Faizan Abbasi <abbasi.faizan1997@gmail.com>
2024-02-21 23:49:01 +0500

    Did some basic express listen and Hello World App

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-const http = require("http");
-const fs = require("fs");
-const url  = require('url')
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()} ${req.url} New request has been recieved\n`;
-  const myUrl = url.parse(req.url, true)
-  console.log(myUrl)
-  fs.appendFile("logs.txt", log, (err, data) => {
-    switch (myUrl.pathname) {
-      case "/":
-        res.end("Home Page");
-        break;
-      case "/about":
-        const myName = myUrl.query.name;
-        res.end(`Hi ${myName}`);
-        break;
-      case "/contact":
-        res.end("contact Page");
-        break;
-    }
-  });
-  // console.log('My requst');
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Home page");
 });
 
-
-myServer.listen(8000, () => {
-  console.log("My server running");
+app.get("/about", (req, res) => {
+  return res.send(`hi ${req.query.name} `);
 });
+
+app.listen(8000, () => console.log("server started"));
