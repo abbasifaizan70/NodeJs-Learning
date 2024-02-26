04aa68b4c90b9a4f7ee428a22151f8e6bdb096f9
Faizan Abbasi <abbasi.faizan1997@gmail.com>
2024-02-26 19:47:03 +0500

    Added Dummy data to make RESTFul APIs

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
+const PORT = 8000;
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  return res.send("Home page");
+// routes
+app.get("/api/users", (req, res) => {
+  return res.json(users);
 });
 
-app.get("/about", (req, res) => {
-  return res.send(`hi ${req.query.name} `);
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Success", id: users.length });
+  });
 });
 
-app.listen(8000, () => console.log("server started"));
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {})
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+    const userToRemoveIndex = users.findIndex(user => {
+      return user.id === id;
+    });
+
+    if (userToRemoveIndex === -1) {
+      return res.json({
+        Error: "No user found with given Id"
+      });
+    } else {
+      users.splice(userToRemoveIndex, 1);
+      fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({
+          Status: "Success",
+          user: `User with ${id} has been removed`
+        });
+      });
+    }
+  });
+
+app.listen(PORT, () => console.log(`Server started at Port: ${PORT}`));
